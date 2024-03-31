8ccb536ff72b95b014be2c2043e377e5513f4115
Emircan ÇUBUKCU <emircancbkc@gmail.com>
2024-03-31 03:45:48 +0300

    navbar-openMenu-add

--- quiz-css/ui/Navbar.jsx
+++ quiz-css/ui/Navbar.jsx
@@ -1,8 +1,13 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
 import { HiMiniBars3BottomRight } from "react-icons/hi2";
+import { IoMdCloseCircleOutline } from "react-icons/io";
 import Style from "./navbar.module.css";
 
 export default function Navbar() {
+
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
         <nav className={Style.nav}>
@@ -11,7 +16,30 @@ export default function Navbar() {
             </div>
 
             <div className={Style.bars}>
-                <button className={Style.btn}><HiMiniBars3BottomRight className={Style.icon} /></button>
+                <button className={Style.btn} onClick={() => setIsOpen((value) => !value)}>{isOpen ? <IoMdCloseCircleOutline className={Style.icon} /> : <HiMiniBars3BottomRight className={Style.icon} />}</button>
+                {isOpen &&
+                    <div className={Style.openMenu}>
+                        <ul className={Style.ulMenu}>
+                            <li className={Style.li}>
+                                <Link className={Style.link} href="/products">Products</Link>
+                            </li>
+                            <li className={Style.li}>
+                                <Link className={Style.link} href="/solutions">Solutions</Link>
+                            </li>
+                            <li className={Style.li}>
+                                <Link className={Style.link} href="/pricing">Pricing</Link>
+                            </li>
+                            <li className={Style.li}>
+                                <Link className={Style.link} href="/resources">Resources</Link>
+                            </li>
+                            <li className={Style.li}>
+                                <Link className={Style.link} href="/login">Log In</Link>
+                            </li>
+                            <li className={Style.li}>
+                                <Link className={`${Style.link} ${Style.signUp}`} href="/signup">Sign up now</Link>
+                            </li>
+                        </ul>
+                    </div>}
             </div>
 
             <div className={Style.right}>
